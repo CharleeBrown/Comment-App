01da2e077d2444eb46dcc745b2042dbd0d834bd0
Coleman <charleebrown22@gmail.com>
2022-10-20 15:35:10 -0500

    Add project files.

---
+++ VB_CommentApp/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SaveBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.CommentBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SaveBtn
+        '
+        Me.SaveBtn.Location = New System.Drawing.Point(111, 257)
+        Me.SaveBtn.Name = "SaveBtn"
+        Me.SaveBtn.Size = New System.Drawing.Size(117, 30)
+        Me.SaveBtn.TabIndex = 0
+        Me.SaveBtn.Text = "Save"
+        Me.SaveBtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name"
+        '
+        'NameBox
+        '
+        Me.NameBox.Location = New System.Drawing.Point(89, 37)
+        Me.NameBox.Name = "NameBox"
+        Me.NameBox.Size = New System.Drawing.Size(233, 27)
+        Me.NameBox.TabIndex = 2
+        '
+        'CommentBox
+        '
+        Me.CommentBox.Location = New System.Drawing.Point(89, 99)
+        Me.CommentBox.Multiline = True
+        Me.CommentBox.Name = "CommentBox"
+        Me.CommentBox.Size = New System.Drawing.Size(233, 118)
+        Me.CommentBox.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Comments"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 332)
+        Me.Controls.Add(Me.CommentBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NameBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SaveBtn)
+        Me.Name = "Form1"
+        Me.Text = "Commentor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaveBtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NameBox As TextBox
+    Friend WithEvents CommentBox As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ VB_CommentApp/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports MongoDB.Driver
+Imports System
+Imports MongoDB.Bson
+Imports System.Runtime
+
+Public Class Form1
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles NameBox.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles CommentBox.TextChanged
+
+    End Sub
+
+    Private Sub SaveBtn_Click(sender As Object, e As EventArgs) Handles SaveBtn.Click
+
+        Dim settings As MongoClientSettings
+        settings = MongoClientSettings.FromConnectionString("mongodb+srv://lee:" + Environment.GetEnvironmentVariable("pwd").ToString() + "@cluster0.guc9f.mongodb.net/?retryWrites=true&w=majority")
+        settings.ServerApi = New ServerApi(ServerApiVersion.V1)
+        Dim client = New MongoClient(settings)
+        Dim database = client.GetDatabase("CommentDB")
+        Dim coll = database.GetCollection(Of BsonDocument)("comments01")
+        Try
+            Dim BsonProps = New BsonDocument().Add("name", NameBox.Text).Add("comment", CommentBox.Text)
+            coll.InsertOne(BsonProps)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        Finally
+            MessageBox.Show("Comment Saved!")
+            NameBox.Clear()
+            CommentBox.Clear()
+        End Try
+
+    End Sub
+End Class
